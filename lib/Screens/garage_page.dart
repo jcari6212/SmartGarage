dad750a669f75e5283edf5daf22424613d8098b9
Mahmoud <mahmoudy1567@gmail.com>
2024-03-03 20:53:28 +0200

    10

--- lib/Screens/garage_page.dart
+++ lib/Screens/garage_page.dart
@@ -83,193 +83,169 @@ class _ChatPageState extends State<ChatPage> with WidgetsBindingObserver {
     setUsername();
     //getRemainingTime();
 
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('messages')
-            .where('id',
-                whereIn: ['$email,$friendEmail', '$friendEmail,$email'])
-            .orderBy('time')
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            List<Message> messagesList = [];
-            for (int i = 0; i < snapshot.data!.docs.length; i++) {
-              messagesList.add(Message.fromJson(snapshot.data!.docs[i]));
-            }
-
-            return Scaffold(
-              appBar: AppBar(
-                automaticallyImplyLeading: false,
-                title: Text(
-                  '$username',
-                  style: const TextStyle(color: Colors.white),
-                ),
-                backgroundColor: kPrimaryColor,
-              ),
-              body: Stack(
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: Text(
+          '$username',
+          style: const TextStyle(color: Colors.white),
+        ),
+        backgroundColor: kPrimaryColor,
+      ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Row(
                 children: [
-                  Column(
-                    children: [
-                      Row(
-                        children: [
-                          CarPacket(
-                            packetNum: Icons.looks_one_rounded,
-                            onTap: () {
-                              pickedPacketNo = '1';
-                              setState(() {
-                                checkoutForm=true;
-                              });
-                            }, used: isRented1,
-                          ),
-                          CarPacket(
-                            packetNum: Icons.looks_two_rounded,
-                            onTap: () {
-                              pickedPacketNo = '2';
-                              setState(() {
-                                checkoutForm=true;
-                              });
-                            }, used: isRented2,
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          CarPacket(
-                            packetNum: Icons.looks_3_rounded,
-                            onTap: () {
-                              pickedPacketNo = '3';
-                              setState(() {
-                                checkoutForm=true;
-                              });
-                            }, used: isRented3,
-                          ),
-                          CarPacket(
-                              packetNum: Icons.looks_4_rounded,
-                              onTap: () {
-                                pickedPacketNo = '4';
-                                setState(() {
-                                  checkoutForm=true;
-                                });
-                              }, used: isRented4,),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          CarPacket(
-                            packetNum: Icons.looks_5_rounded,
-                            onTap: () {
-                              pickedPacketNo = '5';
-                              setState(() {
-                                checkoutForm=true;
-                              });
-                            }, used: isRented5,
-                          ),
-                          CarPacket(
-                            packetNum: Icons.looks_6_rounded,
-                            onTap: () {
-                              pickedPacketNo = '6';
-                              setState(() {
-                                checkoutForm=true;
-                              });
-                            }, used: isRented6,
-                          ),
-                        ],
-                      ),
-                    ],
+                  CarPacket(
+                    packetNum: Icons.looks_one_rounded,
+                    onTap: () {
+                      pickedPacketNo = '1';
+                      setState(() {
+                        checkoutForm=true;
+                      });
+                    }, used: isRented1,
                   ),
-                  Visibility(
-                      visible: checkoutForm,
-                      child: GestureDetector(
-                        onTap: (){
-                          setState(() {
-                            checkoutForm=false;
-                          });
-                        },
-                        child: Container(
-                          color: Colors.black54,
-
-                        ),
-                      )),
-                  Visibility(
-                    visible: checkoutForm,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 280, horizontal: 60),
-                      child: Center(
-                          child: Container(
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(16)),
-                            child: Column(
-                              children: [
-                                Text('Packet No.$pickedPacketNo',style: TextStyle(fontSize: 17),),
-                                Padding(
-                                  padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 26),
-                                  child: TextField(
-                                    onChanged: (data){
-                                      hoursNum = data;
-                                    },
-                                    decoration: InputDecoration(
-                                      fillColor: Colors.white,
-                                      filled: true,
-                                      border: OutlineInputBorder(
-                                        borderSide: const BorderSide(color: kPrimaryColor),
-                                        borderRadius: BorderRadius.circular(12),
-                                      ),
-                                      enabledBorder: OutlineInputBorder(
-                                        borderSide: const BorderSide(color: kPrimaryColor),
-                                        borderRadius: BorderRadius.circular(12),
-                                      ),
-
-                                    ),
-                                  ),
-                                ),
-                                CustomButton2(text: 'Proceed', onTap: (){
-                                  if(hoursNum!=null){
-                                    garage.add({
-                                      'time': DateTime.now().add(Duration(hours: int.parse(hoursNum!))).toString(),
-                                      'id': pickedPacketNo,
-                                    });
-                                    if(pickedPacketNo=='1'){
-                                      isRented1=true;
-                                    }
-                                    if(pickedPacketNo=='2'){
-                                      isRented2=true;
-                                    }
-                                    if(pickedPacketNo=='3'){
-                                      isRented3=true;
-                                    }
-                                    if(pickedPacketNo=='4'){
-                                      isRented4=true;
-                                    }
-                                    if(pickedPacketNo=='5'){
-                                      isRented5=true;
-                                    }
-                                    if(pickedPacketNo=='6'){
-                                      isRented6=true;
-                                    }
-                                    checkoutForm=false;
-                                    hoursNum=null;
-                                  }
-                                })
-                              ],
-                            ),
-                          )),
-                    ),
+                  CarPacket(
+                    packetNum: Icons.looks_two_rounded,
+                    onTap: () {
+                      pickedPacketNo = '2';
+                      setState(() {
+                        checkoutForm=true;
+                      });
+                    }, used: isRented2,
                   ),
                 ],
               ),
-            );
-          } else {
-            return Container(
-                color: Colors.white,
-                child: const Center(
-                    child: Text(
-                  'Loading..',
-                  style: TextStyle(color: kPrimaryColor),
-                )));
-          }
-        });
+              Row(
+                children: [
+                  CarPacket(
+                    packetNum: Icons.looks_3_rounded,
+                    onTap: () {
+                      pickedPacketNo = '3';
+                      setState(() {
+                        checkoutForm=true;
+                      });
+                    }, used: isRented3,
+                  ),
+                  CarPacket(
+                    packetNum: Icons.looks_4_rounded,
+                    onTap: () {
+                      pickedPacketNo = '4';
+                      setState(() {
+                        checkoutForm=true;
+                      });
+                    }, used: isRented4,),
+                ],
+              ),
+              Row(
+                children: [
+                  CarPacket(
+                    packetNum: Icons.looks_5_rounded,
+                    onTap: () {
+                      pickedPacketNo = '5';
+                      setState(() {
+                        checkoutForm=true;
+                      });
+                    }, used: isRented5,
+                  ),
+                  CarPacket(
+                    packetNum: Icons.looks_6_rounded,
+                    onTap: () {
+                      pickedPacketNo = '6';
+                      setState(() {
+                        checkoutForm=true;
+                      });
+                    }, used: isRented6,
+                  ),
+                ],
+              ),
+            ],
+          ),
+          Visibility(
+              visible: checkoutForm,
+              child: GestureDetector(
+                onTap: (){
+                  setState(() {
+                    checkoutForm=false;
+                  });
+                },
+                child: Container(
+                  color: Colors.black54,
+
+                ),
+              )),
+          Visibility(
+            visible: checkoutForm,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                  vertical: 280, horizontal: 60),
+              child: Center(
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16)),
+                    child: Column(
+                      children: [
+                        Text('Packet No.$pickedPacketNo',style: TextStyle(fontSize: 17),),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 26),
+                          child: TextField(
+                            onChanged: (data){
+                              hoursNum = data;
+                            },
+                            decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              border: OutlineInputBorder(
+                                borderSide: const BorderSide(color: kPrimaryColor),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: const BorderSide(color: kPrimaryColor),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+
+                            ),
+                          ),
+                        ),
+                        CustomButton2(text: 'Proceed', onTap: (){
+                          if(hoursNum!=null){
+                            garage.add({
+                              'time': DateTime.now().add(Duration(hours: int.parse(hoursNum!))).toString(),
+                              'id': pickedPacketNo,
+                            });
+                            if(pickedPacketNo=='1'){
+                              isRented1=true;
+                            }
+                            if(pickedPacketNo=='2'){
+                              isRented2=true;
+                            }
+                            if(pickedPacketNo=='3'){
+                              isRented3=true;
+                            }
+                            if(pickedPacketNo=='4'){
+                              isRented4=true;
+                            }
+                            if(pickedPacketNo=='5'){
+                              isRented5=true;
+                            }
+                            if(pickedPacketNo=='6'){
+                              isRented6=true;
+                            }
+                            checkoutForm=false;
+                            hoursNum=null;
+                          }
+                        })
+                      ],
+                    ),
+                  )),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Future<void> setUsername() async {
